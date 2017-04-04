f624d5f0d7ea305db2db32f33dcfc45285a2261e
Alexis Yan <monkey@Alexiss-MacBook-Pro.local>
2017-04-04 12:48:14 -0700

    file structure

---
+++ app.js
@@ -0,0 +1,9 @@
+'use strict';
+console.log("sdjfdkjf");
+
+var sushi = prompt ('do you think I like sushi?');
+if (sushi.trim().toLowerCase() == 'yes'){
+  alert ('you were correct');
+}else {
+alert (' dsfsajkjosi');
+}
